c747a7c3893dea21e0b0a9b11e1bc074f61d4ce6
Adam Ryan <51381387+lazryn@users.noreply.github.com>
2021-04-26 12:58:24 -0400

    Add files via upload

---
+++ Ashley.py
@@ -0,0 +1,13 @@
+﻿from pyvirtualdisplay import Display
+from selenium import webdriver
+
+display = Display(visible=0, size=(1024, 768))
+display.start()
+
+driver = webdriver.Firefox()
+driver.get('https://youtube.com/')
+
+
+driver.quit()
+
+display.stop()
---
+++ SeleniumLoader.py
@@ -0,0 +1,7 @@
+﻿sudo apt-get update
+sudo apt-get install iceweasel
+sudo apt-get install xvfb
+
+sudo pip install selenium
+sudo pip install PyVirtualDisplay
+sudo pip install xvfbwrapper
